b44772ec3782f2b01e44c39ce9845cdc5d801e9d
gmryabov <125265961+gmryabov@users.noreply.github.com>
2024-01-20 20:23:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var cart = document.getElementById("cart");
-
+var cartItems = []
 function getClickedElementId(button) {
     if (button.innerHTML == "Добавить") {
         var father = button.parentNode;
@@ -12,42 +12,103 @@ function getClickedElementId(button) {
         var cost = grproduct.getElementsByClassName("cost")[0];
         if (cart.textContent == "CART") {
             cart.innerHTML = parseInt(cost.textContent)
+            var item = button.parentNode.parentNode.id;
+            cartItems.push(item)
         } else {
             var cartVal = parseInt(cart.textContent) + parseInt(cost.textContent);
             cart.innerHTML = cartVal
+            var item = button.parentNode.parentNode.id;
+            cartItems.push(item)
         }
     }
 }
+
 function plus(element) {
-  var spanElement = element.previousElementSibling;
-  var currentNumber = parseInt(spanElement.textContent);
-  spanElement.textContent = currentNumber + 1
-  var fatherPlus = element.parentNode.parentNode;
-  var cost = parseInt(fatherPlus.getElementsByClassName("cost")[0].textContent);
-  var cartVal = parseInt(cart.textContent)
-  cart.innerHTML = cartVal + cost
+    var spanElement = element.previousElementSibling;
+    var currentNumber = parseInt(spanElement.textContent);
+    spanElement.textContent = currentNumber + 1
+    var fatherPlus = element.parentNode.parentNode;
+    var cost = parseInt(fatherPlus.getElementsByClassName("cost")[0].textContent);
+    var cartVal = parseInt(cart.textContent)
+    cart.innerHTML = cartVal + cost
+    var item = element.parentNode.parentNode.id;
+    cartItems.push(item)
 }
 
 function minus(element) {
-  var spanElement = element.nextElementSibling;
-  var currentNumber = parseInt(spanElement.textContent);
+    var spanElement = element.nextElementSibling;
+    var currentNumber = parseInt(spanElement.textContent);
+    var item = element.parentNode.parentNode.id;
+    var index = cartItems.indexOf(item)
+    if (index !== -1) {
+      cartItems.splice(index, 1);
+    }
 
-  if (currentNumber > 0) {
-    spanElement.textContent = currentNumber - 1;
-  }
-  if (currentNumber < 2) {
-    spanElement.textContent = "Добавить";
-    var father = element.parentNode;
-    var plus = father.getElementsByClassName("plus");
-    var minus = father.getElementsByClassName("minus");
-    plus[0].style.display = 'none';
-    minus[0].style.display = 'none';
-  }
-  var fatherMinus = element.parentNode.parentNode;
-  var cost = parseInt(fatherMinus.getElementsByClassName("cost")[0].textContent);
-  var cartVal = parseInt(cart.textContent)
-  cart.innerHTML = cartVal - cost
-  if (cart.textContent == "0") {
-    cart.innerHTML = "CART"
-  }
+    if (currentNumber > 0) {
+        spanElement.textContent = currentNumber - 1;
+    }
+    if (currentNumber < 2) {
+        spanElement.textContent = "Добавить";
+        var father = element.parentNode;
+        var plus = father.getElementsByClassName("plus");
+        var minus = father.getElementsByClassName("minus");
+        plus[0].style.display = 'none';
+        minus[0].style.display = 'none';
+    }
+    var fatherMinus = element.parentNode.parentNode;
+    var cost = parseInt(fatherMinus.getElementsByClassName("cost")[0].textContent);
+    var cartVal = parseInt(cart.textContent)
+    cart.innerHTML = cartVal - cost
+    if (cart.textContent == "0") {
+        cart.innerHTML = "CART"
+    }
 }
+
+let tg = window.Telegram.WebApp;
+    var cart = document.getElementById("cart");
+    let order = document.getElementById("order");
+
+    cart.addEventListener("click", () =>{
+        document.getElementById("catalog").style.display = "none";
+        document.getElementById("form").style.display = "flex";
+        var cartInner = document.getElementById("cart_inner");
+        cartInner.style.display = "block"
+        document.getElementById("name").value = tg.initDataUnsafe.first_name + " " + tg.initDataUnsafe.last_name
+        console.log(cartItems)
+        var code_html = "";
+        var arr = [];
+        for (var i = 0; i < cartItems.length; i++)  {
+            var target = cartItems[i];
+            var count = 0;
+                for (let i = 0; i < cartItems.length; i++) {
+                    if (target == cartItems[i]) {
+                        count++
+                    }
+                }
+            var prod = document.getElementById(target)
+            var img = prod.querySelector('img');
+            var name = prod.querySelector('h1').textContent;
+            var cost = prod.querySelector('.cost').textContent;
+            if (count == 1) {
+                code_html += "<div class='item'><img class='cart_img' src="+img.src+"><h1 class='cart_item_name'>"+name+"</h1><span class='cost_cart'>"+cost+"</span></div>"
+            } else {
+                if (arr.indexOf(target) !== -1) {
+                    console.log("pass");
+                } else {
+                    arr.push(target)
+                    code_html += "<div class='item'><img class='cart_img' src="+img.src+"><h1 class='cart_item_name'>"+name+"</h1><span class='cost_cart'>"+parseInt(cost)*count+" ₽</span></div>"
+                }
+            }
+        }
+        cartInner.innerHTML = code_html
+    });
+
+    order.addEventListener("click", () =>{
+        tg.close();
+    });
+//cartItems.forEach((id) => console.log(id));
+//cartItems.forEach(function(id) {
+//    var prod = document.getElementById(id)
+//    console.log(prod)
+////    var img = prod.
+//});
