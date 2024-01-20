d863cf10e91bf94f447d22190ce142e749c0c3ff
gmryabov <125265961+gmryabov@users.noreply.github.com>
2024-01-20 16:25:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+var cart = document.getElementById("cart");
+
 function getClickedElementId(button) {
     if (button.innerHTML == "Добавить") {
         var father = button.parentNode;
-        var grproduct = father.parentNode.id;
+        var grproduct = father.parentNode;
         button.innerHTML = 1;
         var plus = father.getElementsByClassName("plus");
         var minus = father.getElementsByClassName("minus");
         plus[0].style.display = 'block';
         minus[0].style.display = 'block';
+        var cost = grproduct.getElementsByClassName("cost")[0];
+        if (cart.textContent == "CART") {
+            cart.innerHTML = parseInt(cost.textContent)
+        } else {
+            var cartVal = parseInt(cart.textContent) + parseInt(cost.textContent);
+            cart.innerHTML = cartVal
+        }
     }
 }
 function plus(element) {
   var spanElement = element.previousElementSibling;
   var currentNumber = parseInt(spanElement.textContent);
-  spanElement.textContent = currentNumber + 1;
+  spanElement.textContent = currentNumber + 1
+  var fatherPlus = element.parentNode.parentNode;
+  var cost = parseInt(fatherPlus.getElementsByClassName("cost")[0].textContent);
+  var cartVal = parseInt(cart.textContent)
+  cart.innerHTML = cartVal + cost
 }
 
 function minus(element) {
@@ -29,5 +42,12 @@ function minus(element) {
     var minus = father.getElementsByClassName("minus");
     plus[0].style.display = 'none';
     minus[0].style.display = 'none';
+  }
+  var fatherMinus = element.parentNode.parentNode;
+  var cost = parseInt(fatherMinus.getElementsByClassName("cost")[0].textContent);
+  var cartVal = parseInt(cart.textContent)
+  cart.innerHTML = cartVal - cost
+  if (cart.textContent == "0") {
+    cart.innerHTML = "CART"
   }
 }
